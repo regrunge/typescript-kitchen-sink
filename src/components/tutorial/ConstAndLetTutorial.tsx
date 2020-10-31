79f6bb9cb5a009b0609e9f8a57808a9e9f2c888b
Sevara Mirzakulova <mirseva55@Sevaras-MacBook-Pro.local>
2020-10-31 16:10:06 +0100

    change title

--- src/components/tutorial/ConstAndLetTutorial.tsx
+++ src/components/tutorial/ConstAndLetTutorial.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 const ConstAndLetTutorial = () => {
-    return (<View><Text>Const and Let</Text></View>);
+    return (<View><Text>Kitchen staff</Text></View>);
 };
 
 export default ConstAndLetTutorial;
