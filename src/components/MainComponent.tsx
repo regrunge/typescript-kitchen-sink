0d6d5b1b8fbe6a42907728e16589531e84ea0675
elvissalaris <elvissalaris@gmail.com>
2021-01-27 15:46:50 +0100

    Timer style

--- src/components/MainComponent.tsx
+++ src/components/MainComponent.tsx
@@ -7,6 +7,10 @@ import {RootStackParamList} from '../navigation/MainStack';
 import List from './Elements/List';
 import Timer from './Elements/Timer';
 import itemStyles from './Elements/styles/itemStyles';
+import {connect} from "react-redux";
+import {ThingType} from "../models/thing";
+import Thing from "../models/thing";
+import {addThing, editThing} from "../redux/dispatch/things";
 
 type MainScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Main'>;
 type MainScreenRouteProp = RouteProp<RootStackParamList, 'Main'>;
@@ -14,28 +18,47 @@ type MainScreenRouteProp = RouteProp<RootStackParamList, 'Main'>;
 type Props = {
   navigation: MainScreenNavigationProp;
   route: MainScreenRouteProp;
+  things: Thing[];
 };
+
+const mapStateToProps = (state: any, ownProps: Props) => {
+    return {
+        things: state.things,
+    };
+};
+
+const mapDispatchToProps = {
+    addThingProp: (thing: Thing) => addThing(thing), // REDUX action
+    editThingProp: (thing: Thing) => editThing(thing), // REDUX action
+};
+
+const connector = connect(mapStateToProps, mapDispatchToProps);
 
 const MainComponent: React.FC<Props> = (props) => {
   const {navigation, route} = props;
   const [showTimer, setShowTimer] = useState(false);
-  const [maxTimer, setMaxTimer] = useState(10);
+  const [maxTimer, setMaxTimer] = useState(69);
 
   const onSelected = (id: string | number | null) => {
+      const thing = props.things.find((t: ThingType) => t.id === id) || { durationMinutes : 0};
     setShowTimer(true);
-    setMaxTimer(12);
+    setMaxTimer(thing.durationMinutes * 60);
   };
 
   const onComplete = () => {
     setShowTimer(false);
-    setMaxTimer(10);
+    setMaxTimer(69);
   };
 
   return (
     <View style={itemStyles.container}>
       <Text>Main Component</Text>
 
-      <Timer show={showTimer} max={maxTimer} onComplete={onComplete} />
+      <Timer
+          show={showTimer}
+          max={maxTimer}
+          onComplete={onComplete}
+      />
 
       <List navigation={navigation} onSelected={onSelected} />
 
@@ -62,4 +85,4 @@ const MainComponent: React.FC<Props> = (props) => {
   );
 };
 
-export default MainComponent;
+export default connector(MainComponent);
